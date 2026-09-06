fae1323035d1b561f7bc84c9f2354c17c0e7f67c
vfxrmalt-lgtm <vfxrmalt@gmail.com>
2026-09-06 19:09:18 -0400

    Create main.tsx

---
+++ project/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { getRouter } from './router'
+
+const router = getRouter()
+const queryClient = router.options.context.queryClient
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+)
